802f0c5c62864067cdd4bda2892bba82c30256cc
jiajiehu <h1721295042@gmail.com>
2021-12-21 20:54:42 +0800

    2.0模糊控制PID

--- USER/pid.c
+++ USER/pid.c
@@ -1,38 +1,187 @@
-#include"pid.h"
-#include"ds18b20.h"
-Pid pid;
+#include "pid.h"
+#include "ds18b20.h"
+FUZZYPID pid;
+FUZZYPID *vPID = &pid;
+const int ruleKp[7][7] = {{PB, PB, PM, PM, PS, ZO, ZO}, {PB, PB, PM, PS, PS, ZO, NS}, {PM, PM, PM, PS, ZO, NS, NS}, {PM, PM, PS, ZO, NS, NM, NM}, {PS, PS, ZO, NS, NS, NM, NM}, {PS, ZO, NS, NM, NM, NM, NB}, {ZO, ZO, NM, NM, NM, NB, NB}};
+const int ruleKi[7][7] = {{NB, NB, NM, NM, NS, ZO, ZO}, {NB, NB, NM, NS, NS, ZO, ZO}, {NB, NM, NS, NS, ZO, PS, PS}, {NM, NM, NS, ZO, PS, PM, PM}, {NM, NS, ZO, PS, PS, PM, PB}, {ZO, ZO, PS, PS, PM, PB, PB}, {ZO, ZO, PS, PM, PM, PB, PB}};
+const int ruleKd[7][7] = {{PS, NS, NB, NB, NB, NM, PS}, {PS, NS, NB, NM, NM, NS, ZO}, {ZO, NS, NM, NM, NS, NS, ZO}, {ZO, NS, NS, NS, NS, NS, ZO}, {ZO, ZO, ZO, ZO, ZO, ZO, ZO}, {PB, NS, PS, PS, PS, PS, PB}, {PB, PM, PM, PM, PS, PS, PB}};
 void pid_init(float set_temperatrue)
 {
-    pid.set_temperature = set_temperatrue;
-    pid.out = 0.0;
-    pid.error = 0.0;
-    pid.error_next = 0.0;
-    pid.error_last = 0.0;
-    //可调节PID 参数。使跟踪曲线慢慢接近阶跃函数200.0 // 
-    pid.kp = KP;
-    pid.ki = KI;
-    pid.kd = KD;
+  pid.set_temperature = set_temperatrue;
+  pid.out = 0.0;
+  pid.error = 0.0;
+  pid.lasterror = 0.0;
+  pid.preerror = 0.0;
+  //可调节PID 参数。使跟踪曲线慢慢接近阶跃函数200.0 //
+  pid.kp = KP;
+  pid.ki = KI;
+  pid.kd = KD;
 }
 
-
-float pid_realise()//实现pid  
+float pid_realise() //实现pid
 {
-	float increment;//增量
-    pid.error = pid.set_temperature - (float)DS18B20_Get_Temp()/10;
-    
-     
-    increment = pid.kp*(pid.error-pid.error_next)+pid.ki*pid.error+pid.kd*(pid.error-2*pid.error_next+pid.error_last);//增量计算公式  
-    
-    pid.out+= increment;
-    
-    if(pid.out>=500){
-        pid.out=500;
-    }else if(pid.out<0){
-        pid.out=0;
-    }
+  float increment; //增量
+  float temperature = (float)DS18B20_Get_Temp() / 10;
+  pid.error = pid.set_temperature - temperature;
 
-    pid.error_last = pid.error_next;//下一次迭代  
-    pid.error_next = pid.error;
-    return pid.out; 
-    
+  increment = pid.kp * (pid.error - pid.lasterror) + pid.ki * pid.error + pid.kd * (pid.error - 2 * pid.lasterror + pid.preerror); //增量计算公式
+
+  pid.out += increment;
+
+  if (pid.out >= 500)
+  {
+    pid.out = 500;
+  }
+  else if (pid.out < 0)
+  {
+    pid.out = 0;
+  }
+
+  pid.preerror = pid.lasterror; //下一次迭代
+  pid.lasterror = pid.error;
+	FuzzyComputation(temperature);
+  return pid.out;
+}
+
+static void LinearQuantization(float pv, float *qValue)
+{
+
+  float thisError;
+
+  float deltaError;
+
+  thisError = vPID->set_temperature - pv; //计算偏差值
+
+  deltaError = thisError - vPID->lasterror; //计算偏差增量
+
+  qValue[0] = 6.0 * thisError / (vPID->maximum - vPID->minimum);
+
+  qValue[1] = 3.0 * deltaError / (vPID->maximum - vPID->minimum);
+}
+
+static void CalcMembership(float *ms, float qv, int *index)
+{
+
+  if ((qv >= -NB) && (qv < -NM))
+
+  {
+
+    index[0] = 0;
+
+    index[1] = 1;
+
+    ms[0] = -0.5 * qv - 2.0; //y=-0.5x-2.0
+
+    ms[1] = 0.5 * qv + 3.0; //y=0.5x+3.0
+  }
+
+  else if ((qv >= -NM) && (qv < -NS))
+
+  {
+
+    index[0] = 1;
+
+    index[1] = 2;
+
+    ms[0] = -0.5 * qv - 1.0; //y=-0.5x-1.0
+
+    ms[1] = 0.5 * qv + 2.0; //y=0.5x+2.0
+  }
+
+  else if ((qv >= -NS) && (qv < ZO))
+
+  {
+
+    index[0] = 2;
+
+    index[1] = 3;
+
+    ms[0] = -0.5 * qv; //y=-0.5x
+
+    ms[1] = 0.5 * qv + 1.0; //y=0.5x+1.0
+  }
+
+  else if ((qv >= ZO) && (qv < PS))
+
+  {
+
+    index[0] = 3;
+
+    index[1] = 4;
+
+    ms[0] = -0.5 * qv + 1.0; //y=-0.5x+1.0
+
+    ms[1] = 0.5 * qv; //y=0.5x
+  }
+
+  else if ((qv >= PS) && (qv < PM))
+
+  {
+
+    index[0] = 4;
+
+    index[1] = 5;
+
+    ms[0] = -0.5 * qv + 2.0; //y=-0.5x+2.0
+
+    ms[1] = 0.5 * qv - 1.0; //y=0.5x-1.0
+  }
+
+  else if ((qv >= PM) && (qv <= PB))
+
+  {
+
+    index[0] = 5;
+
+    index[1] = 6;
+
+    ms[0] = -0.5 * qv + 3.0; //y=-0.5x+3.0
+
+    ms[1] = 0.5 * qv - 2.0; //y=0.5x-2.0
+  }
+}
+
+static void FuzzyComputation(float pv)
+{
+
+  float qValue[2] = {0, 0}; //偏差及其增量的量化值
+
+  int indexE[2] = {0, 0}; //偏差隶属度索引
+
+  float msE[2] = {0, 0}; //偏差隶属度
+
+  int indexEC[2] = {0, 0}; //偏差增量隶属度索引
+
+  float msEC[2] = {0, 0}; //偏差增量隶属度
+
+  float qValueK[3];
+
+  LinearQuantization(pv, qValue);
+
+  CalcMembership(msE, qValue[0], indexE);
+
+  CalcMembership(msEC, qValue[1], indexEC);
+
+  qValueK[0] = msE[0] * (msEC[0] * ruleKp[indexE[0]][indexEC[0]] + msEC[1] * ruleKp[indexE[0]][indexEC[1]])
+
+               + msE[1] * (msEC[0] * ruleKp[indexE[1]][indexEC[0]] + msEC[1] * ruleKp[indexE[1]][indexEC[1]]);
+
+  qValueK[1] = msE[0] * (msEC[0] * ruleKi[indexE[0]][indexEC[0]] + msEC[1] * ruleKi[indexE[0]][indexEC[1]])
+
+               + msE[1] * (msEC[0] * ruleKi[indexE[1]][indexEC[0]] + msEC[1] * ruleKi[indexE[1]][indexEC[1]]);
+
+  qValueK[2] = msE[0] * (msEC[0] * ruleKd[indexE[0]][indexEC[0]] + msEC[1] * ruleKd[indexE[0]][indexEC[1]])
+
+               + msE[1] * (msEC[0] * ruleKd[indexE[1]][indexEC[0]] + msEC[1] * ruleKd[indexE[1]][indexEC[1]]);
+
+  vPID->kp += LinearRealization(vPID->maxdKp, vPID->mindKp, qValueK[0]);
+
+  vPID->ki += LinearRealization(vPID->maxdKi, vPID->mindKi, qValueK[1]);
+
+  vPID->kd += LinearRealization(vPID->maxdKd, vPID->mindKd, qValueK[2]);
+}
+
+static float LinearRealization(float max, float min, float value)
+{
+  return (max - min) * (value + 6) / 12 + min;
 }
--- USER/pid.h
+++ USER/pid.h
@@ -3,17 +3,92 @@
 #define KP 4830.74
 #define KI 70.01
 #define KD 605.5
-void pid_init(float temperature);//PID参数初始化 
-float pid_realise();//实现PID算法   
-typedef struct {
-    float set_temperature;//设定温度 
-    float out;//实际温度
-    float error;//偏差  
-    float error_next;//上一个偏差  
-    float error_last;//上上一个偏差 
-    float kp,ki,kd;//定义比例，积分，微分参数  
+#define NB -6
+#define NM -4
+#define NS -2
+#define ZO 0
+#define PS 2
+#define PM 4
+#define PB 6
+// void pid_init(float temperature);//PID参数初始化 
+// float pid_realise();//实现PID算法   
+// typedef struct {
+//     float set_temperature;//设定温度 
+//     float out;//实际温度
+//     float error;//偏差  
+//     float error_next;//上一个偏差  
+//     float error_last;//上上一个偏差 
+//     float kp,ki,kd;//定义比例，积分，微分参数  
      
-}Pid;
+// }Pid;
 
+typedef struct
+
+{
+
+  float set_temperature;        /*设定值*/
+
+  float kp;                     /*比例系数*/
+
+  float ki;                     /*积分系数*/
+
+  float kd;                     /*微分系数*/
+
+  float error;                  /*当前偏差*/
+
+  float lasterror;              /*前一拍偏差*/
+
+  float preerror;               /*前两拍偏差*/
+
+  float deadband;               /*死区*/
+
+  float out;                    /*输出值*/
+
+  float result;                 /*物理量输出值*/
+
+  float maximum;                /*输出值的上限*/
+
+  float minimum;                /*输出值的下限*/
+
+ 
+
+  float maxdKp;                 /*Kp增量的最大限值*/
+
+  float mindKp;                 /*Kp增量的最小限值*/
+
+  float qKp;                    /*Kp增量的影响系数*/
+
+  float maxdKi;                 /*Ki增量的最大限值*/
+
+  float mindKi;                 /*Ki增量的最小限值*/
+
+  float qKi;                    /*Ki增量的影响系数*/
+
+  float maxdKd;                 /*Kd增量的最大限值*/
+
+  float mindKd;                 /*Kd增量的最小限值*/
+
+  float qKd;                    /*Kd增量的影响系数*/
+
+}FUZZYPID;
+
+
+/*线性量化操作函数,论域{-6，-5，-4，-3，-2，-1，0，1，2，3，4，5，6}*/
+
+static void LinearQuantization(float pv,float *qValue);
+
+
+
+/*隶属度计算函数*/
+
+static void CalcMembership(float *ms,float qv,int * index);
+
+
+/*解模糊化操作,根据具体的量化函数和隶属度函数调整*/
+
+static void FuzzyComputation (float pv);
+
+/*线性化*/
+static float LinearRealization(float max,float min,float value);
 
 #endif
